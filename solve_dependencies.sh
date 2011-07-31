e9433dd409fe8492bf5953d886d4a14d1fbc00b6
Johan Simonsson <github@fun-tech.se>
2011-07-31 13:29:29 +0200

    first check if we need to install anything, then call sudo and install if we really need it

--- solve_dependencies.sh
+++ solve_dependencies.sh
@@ -2,12 +2,19 @@
 
 source include.sh
 
-echo "Install some depends (sudo may ask for your passwd)"
-sudo aptitude install build-essential flex bison \
+package_list="build-essential flex bison \
     libgmp3-dev libmpfr-dev autoconf \
     texinfo libncurses5-dev libexpat1 libexpat1-dev \
-    tk tk8.4 tk8.4-dev
-did_it_work $? 
+    tk tk8.4 tk8.4-dev"
+
+#First check if we need to install anything?
+dpkg -L $package_list 2>&1 > /dev/null 
+if [ ! $? = 0 ]
+then
+    echo "Install some depends (sudo may ask for your passwd)"
+    sudo aptitude install $package_list
+    did_it_work $? 
+fi
 
 echo "Done:"$0
 exit 0
